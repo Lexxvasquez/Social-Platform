986ed9dc5caee9ebd949f6de98ff1c476807d73a
Lexxvasquez <122750837+Lexxvasquez@users.noreply.github.com>
2023-08-17 20:23:23 -0400

    added models

---
+++ models/thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new Schema({
+    thoughttext: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => new Date(createdAtVal).toLocaleDateString()
+    },
+    username: {
+        type: String,
+        require: true,
+    },
+    reactions: [reactionSchema],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
